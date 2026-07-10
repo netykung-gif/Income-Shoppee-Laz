4af66f0c58a8f1dc299673b6e73d874454af5879
netykung-gif <netykung@gmail.com>
2026-07-10 10:37:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -586,18 +586,18 @@ def get_tiktok_expenses_data(files):
 st.set_page_config(page_title="สรุปรายได้/ค่าใช้จ่าย", page_icon="📊", layout="wide")
 st.markdown("""
 <style>
-    .card {
-        background-color: #ffffff;
-        border-radius: 12px;
-        padding: 20px;
-        border: 1px solid #ddd;
-        text-align: center;
-        margin-bottom: 10px;
-        transition: 0.3s;
-    }
-    .card:hover {
-        border: 2px solid #EE4D2D;
-    }
+.card {
+    background-color: #ffffff;
+    border-radius: 12px;
+    padding: 20px;
+    border: 1px solid #ddd;
+    text-align: center;
+    margin-bottom: 10px;
+}
+.card:hover {
+    border: 2px solid #EE4D2D;
+    cursor: pointer;
+}
 </style>
 """, unsafe_html=True)
 
